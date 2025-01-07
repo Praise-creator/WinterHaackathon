0dca737371528f1ec162778e3dcf05a08cfe910e
farihamohamed <alifariha242@gmail.com>
2025-01-06 17:12:45 -0800

    Initial commit with local files for front-end

---
+++ calendar-project/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FileUpload from './components/FileUploadPage';
+
+function App() {
+  return (
+    <div>
+      <h1>File Upload App</h1>
+      <FileUpload />
+    </div>
+  );
+}
+
+export default App;
